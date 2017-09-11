8fbdefc9ec315ef7efacf2e2a8188f9a9b65683f
nice <nice@10.97.60.145>
2017-09-11 14:15:59 +0900

    SHA256 HASH 샘플 추가

--- src/main/java/com/kisline/util/encryptSample.java
+++ src/main/java/com/kisline/util/encryptSample.java
@@ -1,10 +1,61 @@
 package com.kisline.util;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Base64;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class encryptSample {
+	private static final Logger LOGGER = LogManager.getLogger("encryptSample");
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLO WORLD");
+		try {
+			if (args.length != 1) {
+				LOGGER.info("Usage : encryptSample [입력문자]");
+				System.exit(1);
+			}
+
+			String inputValue = args[0];
+
+			LOGGER.info("Plain Text : " + inputValue);
+			LOGGER.info("SHA256 : " + getSHA256(inputValue));
+			LOGGER.info("SHA256(URLEncoded) : " + URLEncoder.encode(getSHA256(inputValue), "UTF-8").toString());
+		} catch (Exception e) {
+			LOGGER.error("main", e);
+		}
+	}
+
+	public static String getSHA256(String s) throws IOException {
+		try {
+			return getCryptoSHA256String(s);
+		} catch (Exception e) {
+			LOGGER.error("getSHA256", e);
+			throw new IOException(e.toString());
+		}
+	}
+
+	private static String getCryptoSHA256String(String inputValue) throws NoSuchAlgorithmException, IOException {
+		if (inputValue == null) {
+			return getCryptoSHA256String("default");
+		}
+		byte[] ret = digest("SHA-256", inputValue.getBytes());
+
+		return encode(ret);
+	}
+
+	private static byte[] digest(String alg, byte[] input) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance(alg);
+		return md.digest(input);
+	}
+
+	private static String encode(byte[] encodeBytes) throws IOException {
+		byte[] buf = Base64.encodeBase64(encodeBytes);
+		return new String(buf).trim();
 	}
 
 }
