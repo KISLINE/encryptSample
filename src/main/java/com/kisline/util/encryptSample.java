156166b8522eecccca317a5c30ade5782833b5fe
nice <nice@10.97.60.145>
2017-09-11 13:54:22 +0900

    init

---
+++ src/main/java/com/kisline/util/encryptSample.java
@@ -0,0 +1,10 @@
+package com.kisline.util;
+
+public class encryptSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+	}
+
+}
